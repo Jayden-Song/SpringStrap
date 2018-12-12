bd294b293e0c7e8e312b6ea1e3636d07c321a58c
yourssong <song@starnation.co.kr>
2018-12-12 15:58:35 +0900

    test 관련 파일 이름 수정

--- src/test/java/com/spraptest/sprap/SpringStrapApplicationTests.java
+++ src/test/java/com/spraptest/sprap/SpringStrapApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wmptest.wmp;
+package com.spraptest.sprap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WmpApplicationTests {
+public class SpringStrapApplicationTests {
 
 	@Test
 	public void contextLoads() {
